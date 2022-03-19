ce0162433e5281ecddec56d205790b9e78b65770
abhashs <abhashshrestha@outlook.com>
2022-03-18 17:12:29 -0700

    Scan state fix

--- src/btree/IndexFile.java
+++ src/btree/IndexFile.java
@@ -8,12 +8,12 @@ import global.*;
 /**
  * Contains the enumerated types of state of the scan
  */
-class ScanState 
-{
-  public static final int NEWSCAN = 0; 
-  public static final int SCANRUNNING = 1; 
-  public static final int SCANCOMPLETE = 2; 
-}
+// class ScanState 
+// {
+//   public static final int NEWSCAN = 0; 
+//   public static final int SCANRUNNING = 1; 
+//   public static final int SCANCOMPLETE = 2; 
+// }
 
 /**
  * Base class for a index file
